bb900b4e57eb1e9e7f16d8308247f3de3bd24d0b
YosraQA <yosrawelhazi@yahoo.fr>
2022-08-05 17:49:06 +0100

    Sixth Update

--- src/test/java/com/e2etests/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2etests/automation/RunWebSuiteTest.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @CucumberOptions(
 		features= {"src/test/features"},
 		plugin={"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
-		tags = ("@job_list"),
+		tags = ("@authentification"),
 		monochrome = true,
 		snippets = CAMELCASE
 		)
